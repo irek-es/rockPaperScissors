4e7f2114df4edad403a36e3026058c5b552be602
Irek <irek.swara@protonmail.com>
2022-11-28 15:54:55 +0000

    Function get user input and checking if correct

--- main.js
+++ main.js
@@ -6,9 +6,34 @@ function getComputerChoice () {
     let choice = words[Math.floor(Math.random() * 3)];
 
     //test
-    //console.log(choice);
+    console.log("Computer: " + choice);
+
 }
 
 //player choice
+function getPlayerChoice () {
+    //user input
+    let pickOne = prompt("Your turn. Rock, Paper or Scissors?");
+    //change input string to lower case
+    pickOne = pickOne.toLowerCase();
+    //check user input
+    if (pickOne == "rock") {
+        pickOne = "Rock"
+    } else if (pickOne == "paper") {
+        pickOne = "Paper";
+    } else if (pickOne == "scissors") {
+        pickOne = "Scissors";
+    } else {
+        console.log("Incorrect input. Chose between rock, paper and scissors.");
+        getPlayerChoice();
+    }
+    
+    //test
+    console.log("Player: " + pickOne);
+}
 
 //play single round
+
+//test
+getPlayerChoice();
+getComputerChoice();
