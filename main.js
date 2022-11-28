65af69364dba2f69562c093a770cc69718111143
Irek <irek.swara@protonmail.com>
2022-11-28 15:31:14 +0000

    Random computer choice

---
+++ main.js
@@ -0,0 +1,14 @@
+//computer choice, randomly rock, paper or scissors
+function getComputerChoice () {
+    //possible choices
+    const words = ['Rock', 'Paper', 'Scissors']
+    //random number to pick from array
+    let choice = words[Math.floor(Math.random() * 3)];
+
+    //test
+    //console.log(choice);
+}
+
+//player choice
+
+//play single round
