13ca29ad58a2d3a05546fbc5a389c841b978f5f3
Irek <irek.swara@protonmail.com>
2022-11-30 22:41:41 +0000

    Function allowing to play one round and indicate the winner

--- main.js
+++ main.js
@@ -31,7 +31,7 @@ function getPlayerChoice () {
     }
     
     //test
-    //console.log("Player: " + pickOne);
+    console.log("Player: " + pickOne);
 
     //return choice value to use later
     return pickOne;
@@ -39,13 +39,35 @@ function getPlayerChoice () {
 
 //play single round
 function playRound() {
-    console.log(playerSelection);
-    console.log(computerSelection);
+    //parameters for function playRound
+    let playerSelection = getPlayerChoice();
+    let computerSelection = getComputerChoice();
+
+    //find who wins 1.1 / use old one just change first letter to capital
+    if (playerSelection === computerSelection) {
+        console.log("Draw. Play again");
+        playRound();
+    } else if (playerSelection === "Paper") {
+        if (computerSelection === "Rock") {
+            console.log("User win. Paper wrap rock.");
+        } else {
+            console.log("Computer win. Scissors cut paper.");
+        }
+    } else if (playerSelection === "Rock") {
+        if (computerSelection === "Paper") {
+            console.log("Computer win. Paper wrap rock.");
+        } else {
+            console.log("User win. Rock nick scissors.");
+        }
+    } else if (playerSelection === "Scissors") {
+        if (computerSelection === "Rock") {
+            console.log("Computer win. Rock nick scissors.");
+        } else {
+            console.log("Player win. Scissors cut paper.");
+        }
+    } else {
+        console.log("something misssing");
+    }
 }
-
-//parameters for function playRound
-let playerSelection = getPlayerChoice();
-let computerSelection = getComputerChoice();
-
 //test
 playRound();
